27feb5cdf0d3633a7f673ef2dc4b936b144cd9fe
Thomas-S-B <contact@struller-baumann.de>
2013-09-26 09:19:22 +0200

    Formatting

--- src/main/java/de/strullerbaumann/visualee/examiner/Examiner.java
+++ src/main/java/de/strullerbaumann/visualee/examiner/Examiner.java
@@ -9,9 +9,9 @@ package de.strullerbaumann.visualee.examiner;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -159,7 +159,6 @@ public abstract class Examiner {
          cleanedClassName = className.substring(0, className.indexOf('['));
          arrayToken = className.substring(className.indexOf('['));
       }
-
       switch (cleanedClassName) {
          case "boolean":
             cleanedClassName = "Boolean";
@@ -186,7 +185,6 @@ public abstract class Examiner {
             cleanedClassName = "Double";
             break;
       }
-
       return cleanedClassName + arrayToken;
    }
 
